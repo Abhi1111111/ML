98504d5c78862f323af44acf508ce758b1bd0bad
Abhi1111111 <101495388+Abhi1111111@users.noreply.github.com>
2024-04-11 09:39:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
 st.write("hello world")
 x=st.text_input("favourite movie?")
-st.write(f"favarite movie is {x}")
+st.write(f"favourite movie is {x}")
+is_clicked=st.button("click me")
